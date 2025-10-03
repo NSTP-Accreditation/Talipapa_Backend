40fb7ca597764d9158081c39ef7942054cf26766
Ryanny <ryannymromero@gmail.com>
2025-10-04 00:30:37 +0800

    Make post endpoint for news

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 5555;
-const connDB = require('./config/connDB');
+const connDB = require('./config/connDB')
 const mongoose = require('mongoose');
 
 // This is to use the .env files
@@ -11,20 +11,16 @@ require('dotenv').config;
 // Connect to db
 connDB();
 
-
-console.log(process.env.SAMPLE);
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 // app.use(express.static(path.join(__dirname, 'public'))); // Optional since wala pa namang public directory
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-})
+// ENDPOINTS
+app.use('/news', require('./routes/api/newsRoute'))
 
 mongoose.connection.once("open", () => {
   console.log('Connected To DB');
 
   app.listen(port, () => console.log(`Server is listening to port ${port}`));
 });
-
