c50900df97c7bfa81e6ce19b203bbbc46acc58d5
Ryanny <ryannymromero@gmail.com>
2025-10-04 19:25:47 +0800

    Handle Account Creation Error Properly

--- controller/authController.js
+++ controller/authController.js
@@ -1,18 +1,38 @@
-const User = require('../model/User');
-const bcrypt = require('bcrypt');
+const User = require("../model/User");
+const bcrypt = require("bcrypt");
 
 const handleCreateAccount = async (request, response) => {
   const { username, email, contactNumber, address, password } = request.body;
 
-  if (!username || !email | !contactNumber || !address || !password ) return response.status(400).json({ message: "Username, Email, Contact Number Address and Password is required!"});
+  if (!username || !email | !contactNumber || !address || !password)
+    return response.status(400).json({
+      message:
+        "Username, Email, Contact Number Address and Password is required!",
+    });
 
-  // TODO: Validate the email, and contact Number
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  if (!emailRegex.test(email)) {
+    return response.status(400).json({ message: "Invalid Email Format!" });
+  }
+
+  if(contactNumber.length !== 11) {
+    return response.status(400).json({ message: "Invalid Contact Number!" });
+  }
 
   try {
-    const foundUser = await User.findOne({ $or: [{ username }, { email }] }).lean();
+    const foundUser = await User.findOne({
+      $or: [{ username }, { email }],
+    }).lean();
 
-    if(foundUser) return response.status(409).json({ message: "User Already Exists"});
-    
+    if (foundUser) {
+      if (foundUser.username === username) {
+        return response.status(409).json({ message: "Username Already Exists" });
+      }
+      if (foundUser.email === email) {
+        return response.status(409).json({ message: "Email Already Exists" });
+      }
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const newUser = await User.create({
@@ -21,19 +41,17 @@ const handleCreateAccount = async (request, response) => {
       // roles,
       contactNumber,
       address,
-      password: hashedPassword
+      password: hashedPassword,
     });
-    
-    response.status(201).json({ message: `User ${newUser.username} created successfully!`});
+
+    response
+      .status(201)
+      .json({ message: `User ${newUser.username} created successfully!` });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-}
+};
 
-const handleLogin = (request, response) => {
-
-
-
-}
+const handleLogin = (request, response) => {};
 
 module.exports = { handleCreateAccount, handleLogin };
