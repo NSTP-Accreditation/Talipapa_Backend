05133b742acdc7ff964e7dace1a07031881b341e
Ryanny <ryannymromero@gmail.com>
2025-10-04 19:49:13 +0800

    Make a Login Endpoint

--- controller/authController.js
+++ controller/authController.js
@@ -1,5 +1,6 @@
 const User = require("../model/User");
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 
 const handleCreateAccount = async (request, response) => {
   const { username, email, contactNumber, address, password } = request.body;
@@ -52,6 +53,46 @@ const handleCreateAccount = async (request, response) => {
   }
 };
 
-const handleLogin = (request, response) => {};
+const handleLogin = async (req, res) => {
+  const { username, password } = req.body;
+  if(!username || !password) return res.status(400).json({'error': 'Username and Password are required!'});
+
+  const foundUser = await User.findOne({ username }).exec();
+  if(!foundUser) return res.status(401).json({ error: 'Username or Password is incorrect.'});
+
+  try {
+    const match = await bcrypt.compare(password, foundUser.password);
+
+    if(match) {
+      const accessToken = jwt.sign(
+        { username: foundUser.username },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: process.env.ACCESS_TOKEN_EXP }
+      )
+      const refreshToken = jwt.sign(
+        { username: foundUser.username },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: process.env.ACCESS_TOKEN_EXP }
+      )
+      
+      foundUser.refreshToken = refreshToken;
+      await foundUser.save()
+      
+      res.cookie('jwt', refreshToken, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000, secure: false, sameSite: "None"}); //  Secure True, Samesite: Strict 
+      
+      res.json({
+        'userData': {
+          username: foundUser.username,
+        },
+        'accessToken': accessToken
+      });
+    } else {
+      res.status(401).json({ error: 'Username or Password is incorrect.' });
+    }
+  } catch (error) {
+    res.status(500).json({'error': error.message})
+  }
+
+};
 
 module.exports = { handleCreateAccount, handleLogin };
--- model/User.js
+++ model/User.js
@@ -21,7 +21,8 @@ const userSchema = new Schema({
   password: {
     type: String,
     required: true
-  }
+  },
+  refreshToken: String
 })
 
 module.exports = mongoose.model("User", userSchema);
