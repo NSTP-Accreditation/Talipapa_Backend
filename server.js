2f24840b052f3a3f4cc9ee023da28b5b4a377300
Bryan <brynayan26@gmail.com>
2025-10-04 19:56:22 +0800

    Make Page Content end point

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 const port = 5555;
-const connDB = require('./config/connDB')
-const mongoose = require('mongoose');
+const connDB = require("./config/connDB");
+const mongoose = require("mongoose");
 
 // This is to use the .env files
-require('dotenv').config();
+require("dotenv").config();
 
 // Connect to db
 connDB();
 
 // middlewares
 app.use(express.json());
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 // app.use(express.static(path.join(__dirname, 'public'))); // Optional since wala pa namang public directory
 
 // ENDPOINTS
-app.use('/news', require('./routes/api/newsRoute'));
+app.use("/news", require("./routes/api/newsRoute"));
+
+app.use("/pagecontent", require("./routes/api/PageContentRoute"));
 
 mongoose.connection.once("open", () => {
-  console.log('Connected To DB');
+  console.log("Connected To DB");
 
   app.listen(port, () => console.log(`Server is listening to port ${port}`));
 });
