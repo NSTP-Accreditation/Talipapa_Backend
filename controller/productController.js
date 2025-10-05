891d5e432bd791752a9fbaaf311269d2ae3ba886
Ryanny <ryannymromero@gmail.com>
2025-10-05 14:29:48 +0800

    Make Get and Create Product Endpoint

---
+++ controller/productController.js
@@ -0,0 +1,33 @@
+const Product = require('../model/Products');
+
+// TODO: ADD IMAGE
+const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    if(!products) return res.status(204).json({'message': 'No Products found'});    
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({error: error.message});
+  }
+};
+
+const createProduct = async (req, res) => {
+  const { name, description, category, subCategory, requiredPoints } = req.body;
+  
+  if(!name || !description || !category || !subCategory || !requiredPoints ) return res.status(400).json({ 'error': "All Fields are required!"});
+  
+  try {
+    const newProduct = await Product.create({
+      name,
+      description,
+      category,
+      subCategory,
+      requiredPoints
+    });
+    res.status(201).json({ message: `Product ${newProduct.name} Created!`});
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = { getProducts, createProduct };
---
+++ model/Products.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose;
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  subCategory: {
+    type: String,
+    required: true
+  },
+  requiredPoints: {
+    type: Number,
+    required: true
+  },
+  image: {
+    type: String,
+  }
+});
+
+
+module.exports = mongoose.model('Product', productSchema);
