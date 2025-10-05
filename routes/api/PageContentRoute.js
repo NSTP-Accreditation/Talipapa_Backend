7861e9ad3f839a12debc7eab0837d570f4cc1ff0
Bryan <brynayan26@gmail.com>
2025-10-05 23:56:20 +0800

    CHANGE CONTROLLER PageContent/Endpoint

--- routes/api/PageContentRoute.js
+++ routes/api/PageContentRoute.js
@@ -1,77 +1,17 @@
 const express = require("express");
+const {
+  getAllPageContents,
+  postPageContents,
+  updatePageContents,
+} = require("../../controller/pageContentController");
+
 const router = express.Router();
-const PageContent = require("../../model/PageContent");
 
-// GET all page contents
-router
-  .route("")
-  .get(async (request, response) => {
-    try {
-      const allContent = await PageContent.find({});
-
-      response.json(allContent);
-    } catch (error) {
-      response.status(500).json({ error: error.message });
-    }
-  })
-  .post(async (request, response) => {
-    const { mission, vision } = request.body;
-
-    try {
-      const allContent = await PageContent.find({});
-      if(allContent.length > 0) {
-        if(mission || vision) {
-          return response
-          .status(400)
-          .json({ message: "Mission and Vision content is already defined!" });
-        }
-      }
-
-      if (!mission || !vision)
-        return response
-          .status(400)
-          .json({ message: "Mission and Vision content are required!" });
-
-      const pageContent = await PageContent.create({
-        mission,
-        vision,
-      });
-
-      response.status(201).json(pageContent);
-    } catch (error) {
-      response.status(500).json({ error: error.message });
-    }
-  });
+router.route("")
+      .get(getAllPageContents)
+      .post(postPageContents);
 
 router
-  .route("/:id")
-  .put(async (request, response) => {
-    const { id } = request.params;
-    const { mission, vision } = request.body;
-
-    if (!id)
-      return response.status(400).json({ message: "The ID is required!" });
-
-    try {
-      if (!mission || !vision)
-        return response
-          .status(400)
-          .json({ message: "Mission and Vision are required!" });
-
-      const updatedContent = await PageContent.findByIdAndUpdate(
-        { _id: id },
-        {
-          mission,
-          vision,
-          updated_at: new Date(),
-        },
-        { new: true }
-      );
-
-      response.json(updatedContent);
-    } catch (error) {
-      response.status(500).json({ error: error.message });
-    }
-  })
+.route("/:id").put(updatePageContents);
 
 module.exports = router;
