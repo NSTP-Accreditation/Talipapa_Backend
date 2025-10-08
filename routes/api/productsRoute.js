9f43f18d660709675a3a391748024c91f5a73f37
Ryanny <ryannymromero@gmail.com>
2025-10-08 22:53:59 +0800

    Make Delete Product Endpoint

--- routes/api/productsRoute.js
+++ routes/api/productsRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getProducts, createProduct, updateProduct } = require('../../controller/productController');
+const { getProducts, createProduct, updateProduct, deleteProduct } = require('../../controller/productController');
 const verifyJWT = require('../../middlewares/verifyJWT')
 const roles = require('../../config/roles');
 const verifyRoles = require('../../middlewares/verifyRoles');
@@ -9,8 +9,9 @@ const verifyRoles = require('../../middlewares/verifyRoles');
 router.route('/')
   .get(getProducts)
   .post(verifyJWT, verifyRoles(roles.Admin, roles.SuperAdmin), createProduct)
-// TODO: MAKE UPDATE and DELELTE PRODUCT
 
 router.route("/:id")
-  .put(verifyJWT, verifyRoles(roles.Admin, roles.SuperAdmin), updateProduct)
+  .all(verifyJWT, verifyRoles(roles.Admin, roles.SuperAdmin))
+  .put(updateProduct)
+  .delete(deleteProduct)
 module.exports = router;
