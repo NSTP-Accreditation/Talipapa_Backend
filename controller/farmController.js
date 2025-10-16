07b1500fc9724b302397e2a0c492d44861914cc1
Ryanny <ryannymromero@gmail.com>
2025-10-16 12:46:43 +0800

    aggregate members for getFarms

--- controller/farmController.js
+++ controller/farmController.js
@@ -1,27 +1,55 @@
-const { LOGCONSTANTS } = require('../config/constants');
-const Farm = require('../model/Farm');
+const { LOGCONSTANTS } = require("../config/constants");
+const Farm = require("../model/Farm");
 
 const getFarms = async (req, res) => {
   try {
-    const farms = await Farm.find().lean().exec();
-    if(!farms || farms.length === 0) return res.sendStatus(204);
+    const farms = await Farm.aggregate([
+      {
+        $lookup: {
+          from: "staffs",
+          localField: "_id",
+          foreignField: "assigned_farm",
+          as: "members",
+        },
+      },
+      {
+        $addFields: {
+          memberCount: { $size: "$members" },
+        },
+      },
+      {
+        $project: {
+          members: 0, 
+        },
+      },
+    ]);
+
     res.json(farms);
   } catch (error) {
     res.status(500).json({ error: err.message });
   }
 };
 
-  
 const addFarm = async (req, res) => {
-  const { location, name, size, age, farmType, address, description } = req.body;
+  const { location, name, size, age, farmType, address, description } =
+    req.body;
 
-  if(!location || !name || !size || !age || !farmType || !address || !description) return res.status(400).json({ error: 'All Fields are required!'});
+  if (
+    !location ||
+    !name ||
+    !size ||
+    !age ||
+    !farmType ||
+    !address ||
+    !description
+  )
+    return res.status(400).json({ error: "All Fields are required!" });
   const parsedLocation = JSON.parse(req.body.location);
   try {
     const newFarm = await Farm.create({
       location: {
         lat: Number(parsedLocation.lat),
-        lng: Number(parsedLocation.lng)
+        lng: Number(parsedLocation.lng),
       },
       name,
       size,
@@ -35,7 +63,7 @@ const addFarm = async (req, res) => {
     await createLog({
       action: LOGCONSTANTS.actions.farms.CREATE_FARM,
       category: LOGCONSTANTS.categories.GREEN_PAGE,
-      title: 'New Farm Created',
+      title: "New Farm Created",
       description: `Farm ${name} created successfully`,
       performedBy: req.userId,
       targetType: LOGCONSTANTS.targetTypes.FARM,
@@ -43,19 +71,14 @@ const addFarm = async (req, res) => {
       targetName: newFarm.name,
     });
 
-    res.status(201).json({ message: 'New Farm Added!'});
+    res.status(201).json({ message: "New Farm Added!" });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
-const updateFarm = async (req, res) => {
+const updateFarm = async (req, res) => {};
 
-};
-
-const deleteFarm = async (req, res) => {
-
-};
-
+const deleteFarm = async (req, res) => {};
 
 module.exports = { getFarms, addFarm, updateFarm, deleteFarm };
