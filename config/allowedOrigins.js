b8aef30bfb821ee424a5399a795f11fa59e6d0b3
Roven-pogi <rovenburasca@gmail.com>
2025-10-22 10:05:28 +0800

    refactcasdas

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
   "http://localhost:5555",
   "http://localhost:5173",
   "http://localhost:3000",
-  "https://ecocycletalipapa.com" /*" Domain Endpoint "*/,
+  "https://www.ecocycletalipapa.com" /*" Domain Endpoint "*/,
 ];
 
 module.exports = allowedOrigins;
