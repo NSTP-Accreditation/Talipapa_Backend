c4db01e2592cb3e3badf2ac1ae1913c209e7c3ee
Ryanny <ryannymromero@gmail.com>
2025-11-16 12:44:48 +0800

    add establishment ID

--- model/Establishment.js
+++ model/Establishment.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+const EstablishmentCounter = require('./EstablishmentCounter');
 const { Schema } = mongoose;
 
-
 const establishmentSchema = new Schema({
+  _id: String,
   name: {
     type: String,
     required: true,
@@ -25,5 +26,24 @@ const establishmentSchema = new Schema({
   },
 }, { timestamps: true });
 
+establishmentSchema.pre('save', async function (next) {
+  if (this.isNew) {
+    try {
+      const counter = await EstablishmentCounter.findByIdAndUpdate(
+        'establishmentId',
+        { $inc: { seq: 1 } },
+        { new: true, upsert: true }
+      );
+
+      this._id = `BTE-${String((counter.seq)).padStart(4, '0')}`;
+      
+      next();
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    next();
+  }
+});
 
 module.exports = mongoose.model('Establishment', establishmentSchema);
